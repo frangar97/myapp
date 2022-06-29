2eb44efa81bab6631ba64e9ed003313f001d7277
Francisco A. Garcia <frangarcia38@hotmail.com>
2022-06-29 16:42:33 -0600

    Se añade seccion para probar paginas

--- routes.go
+++ routes.go
@@ -10,6 +10,8 @@ func (a *application) routes() *chi.Mux {
 
 	//Rutas
 	a.App.Routes.Get("/", a.Handlers.Home)
+	a.App.Routes.Get("/go-page", a.Handlers.GoPage)
+	a.App.Routes.Get("/jet-page", a.Handlers.JetPage)
 
 	//rutas estaticas
 	fileServer := http.FileServer(http.Dir("./public"))
